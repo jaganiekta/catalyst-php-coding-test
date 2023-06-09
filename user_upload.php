e1d1883ed837a920963413315e7605badaf982cc
jaganiekta <ekta.hansaliya@gmail.com>
2023-06-09 08:46:38 +0530

    Mysql connection and create table code

--- user_upload.php
+++ user_upload.php
@@ -1,7 +1,7 @@
 <?php
 
 // Command line options
-$options = getopt("u:p:h:", ["file:", "create_table", "dry_run", "help"]);
+$options = getopt("u:p:h:d:", ["file:", "create_table", "dry_run", "help"]);
 
 // Help message
 $helpMessage = "
@@ -26,3 +26,48 @@ if (isset($options['help'])) {
     exit;
 }
 
+// Validate and process options
+$file = $options['file'] ?? '';
+$createTable = isset($options['create_table']);
+$dryRun = isset($options['dry_run']);
+$username = $options['u'] ?? 'root';
+$password = $options['p'] ?? '';
+$host = $options['h'] ?? 'localhost';
+$dbname = $options['d'] ?? 'admin';
+
+// Connect to MySQL server
+$mysqli = new mysqli($host, $username, $password);
+if ($mysqli->connect_errno) {
+    echo "Error: Failed to connect to MySQL: " . $mysqli->connect_error . "\n";
+    exit(1);
+}
+
+$createDbSql = "CREATE DATABASE IF NOT EXISTS " . $dbname;
+if ($mysqli->query($createDbSql) === TRUE) {
+   echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $mysqli->error;
+  exit(1);
+}
+
+// Create or rebuild the users table if requested
+mysqli_select_db($mysqli, $dbname);
+if ($createTable) {
+    $createTableQuery = "
+        CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            surname VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL
+        )
+    ";
+
+    if ($mysqli->query($createTableQuery) === true) {
+        echo "Table 'users' created successfully.\n";
+    } else {
+        echo "Error: Failed to create table 'users': " . $mysqli->error . "\n";
+        exit(1);
+    }
+
+    exit;
+}
